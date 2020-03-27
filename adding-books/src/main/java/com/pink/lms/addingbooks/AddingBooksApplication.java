98664b5de3ab12f8c6e5bb33b59e15787cc9503d
Rajpal Singh <rajpsinghbajwa@gmail.com>
2020-03-27 19:10:22 -0400

    Initial Commit for Adding Book service

---
+++ adding-books/src/main/java/com/pink/lms/addingbooks/AddingBooksApplication.java
@@ -0,0 +1,13 @@
+package com.pink.lms.addingbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddingBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddingBooksApplication.class, args);
+	}
+
+}
---
+++ adding-books/src/test/java/com/pink/lms/addingbooks/AddingBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pink.lms.addingbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddingBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
